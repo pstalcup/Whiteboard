3e92cb87e6c01dc93f14aab55c040b5dbb2f4031
Ashby <gbowles@umw.edu>
2011-02-23 20:04:00 -0500

    this better work

--- white_board.sql
+++ white_board.sql
@@ -3,12 +3,13 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 20, 2011 at 08:26 PM
+-- Generation Time: Feb 23, 2011 at 07:39 PM
 -- Server version: 5.5.8
 -- PHP Version: 5.3.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
+GRANT ALL ON white_board.* to 'whiteboard'@'localhost' IDENTIFIED BY 'password';
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,6 +19,46 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Database: `white_board`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group`
+--
+
+CREATE TABLE IF NOT EXISTS `group` (
+  `group name` varchar(10) NOT NULL,
+  `group description` blob NOT NULL,
+  `group admin` varchar(10) NOT NULL,
+  PRIMARY KEY (`group name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `group`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
+CREATE TABLE IF NOT EXISTS `news` (
+  `key` int(11) NOT NULL AUTO_INCREMENT,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `title` varchar(20) NOT NULL,
+  `value` blob NOT NULL,
+  `author` varchar(20) NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`key`, `time`, `title`, `value`, `author`) VALUES
+(1, '2011-02-23 19:36:30', 'Mid Term Exam', 0x496d20667265616b696e67206f75742121, 'Thomas Jefferson');
 
 -- --------------------------------------------------------
 
@@ -35,6 +76,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
+INSERT INTO `users` (`userName`, `password`) VALUES
+('Dr.Pepper', 'password');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
