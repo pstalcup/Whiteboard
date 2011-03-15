ef877fe410f17e070d9ff0ca40869944d95b2dbd
Ashby <gbowles@umw.edu>
2011-03-14 22:27:30 -0400

    Started group profiles and added new white board and emails

---
+++ groupProf.php
@@ -0,0 +1,17 @@
+<?php include "db_connect.php"?>
+
+<?php
+$query= "SELECT * FROM group";
+$result= mysqli_query($db, $query)
+or die ('Error querying te database.');
+
+while ($row = mysqli_fetch_array($result)) {
+
+$group_name = $row['name'];
+$group_desc = $row['description'];
+$group_admin = $row['admin'];
+
+echo 'Group:'.$group_name.'</br>'
+     'Admin:'.$group_admin.'</br>'
+	 'Info:'.$group_desc.'</br>';
+	 }
