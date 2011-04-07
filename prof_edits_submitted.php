9525aae175deb5466e126b79f72432dd49681cfc
Ashby <gbowles@umw.edu>
2011-04-07 00:40:07 -0400

    change password

--- prof_edits_submitted.php
+++ prof_edits_submitted.php
@@ -5,10 +5,10 @@
    $email = $_POST['email'];
    $avatar = $_POST['avatar']; 
    $name = $_SESSION['username'];
+   $password = $_POST['password'];
    
-   $query= "UPDATE users SET Bio = '$bio', eMail = '$email', Avatar = '$avatar' WHERE userName = '$name'";
-      
-   echo($query);
+   $query= "UPDATE users SET Bio = '$bio', eMail = '$email', Avatar = '$avatar', password = '$password' WHERE userName = '$name'";
+     
    
    mysqli_query($db,$query) or die("Query Failed LOLZ");
    
